1f0530ae63c0f3a660b4fb717b70f39d736ac4e8
selvi <selvi.alberts@gmail.com>
2022-10-04 23:18:20 +1000

    Initial version of subtract program

---
+++ substract_procedural.py
@@ -0,0 +1,18 @@
+
+# Get the first number
+x = 20
+# Get the second number
+y = 30
+# Subtract two numbers and store result
+z= x-y
+# print result
+print (z)
+
+# Get the first number
+x = 18
+# Get the second number
+y = 20
+# add two numbers and store result
+z= x+y
+# print result
+print (z)
