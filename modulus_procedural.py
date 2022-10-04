d7f642312748050ade3f89e4acc0c9c469204a5a
selvi <selvi.alberts@gmail.com>
2022-10-04 23:24:38 +1000

    Initial version of % program

---
+++ modulus_procedural.py
@@ -0,0 +1,18 @@
+
+# Get the first number
+x = 5
+# Get the second number
+y = 2
+#  Divide two numbers and store quotient
+z= x%y
+# print result
+print (z)
+
+# Get the first number
+x = 6
+# Get the second number
+y = 4
+#  Divide two numbers and store quotient
+z= x%y
+# print result
+print (z)
