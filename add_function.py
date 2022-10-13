d1ee7e30e2653f88c92a2a033c7e4906f5f69e20
selvi <selvi.alberts@gmail.com>
2022-10-13 21:06:11 +1000

    Updated version of add_function.py

--- add_function.py
+++ add_function.py
@@ -2,11 +2,11 @@
 def add_numbers(x,y):
     x = input("Enter first value")
     y = input("Enter Second Value")
-    if (int(x)<=0 or int(y)<=0):
+    if (int(x) <= 0 or int(y) <= 0):
         print(" please enter a positive number")
         exit()
     else:
-         z = int(x) + int(y)
-         print (int(z))
+        z = int(x) + int(y)
+        print(x, "+", y, "=", int(z))
 
 add_numbers(10,20)
