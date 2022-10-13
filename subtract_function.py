fead3b55338a8e578167fafc06a7b129c86906a0
selvi <selvi.alberts@gmail.com>
2022-10-13 21:06:47 +1000

    Updated version of subtract_function.py

--- subtract_function.py
+++ subtract_function.py
@@ -1,16 +1,13 @@
 
 def subtract_numbers(x,y):
-    z = int(x) - int(y)
-    print(int(z))
-
     x = input("Enter first value")
     y = input("Enter Second Value")
     if (int(x)<=0 or int(y)<=0):
         print(" please enter a positive number")
         exit()
-    else:
-         z = int(x) - int(y)
-         print(int(z))
+    else :
+        z = int(x) - int(y)
+        print(x,"-",y,"=",int(z))
 
 subtract_numbers(10,20)
 
