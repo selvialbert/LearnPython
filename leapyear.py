fa7fdf0734c06de15b7acbfbb1877e8be4cd353f
selvi <selvi.alberts@gmail.com>
2022-10-20 20:28:31 +1000

    Initial version procedural for leap year

--- leapyear.py
+++ leapyear.py
@@ -1,10 +1,20 @@
 # Program to find out if a year is a leap year
 
 # step 1: Get the input from user
+year = int(input("Enter an Year : "))
+
 # step 2 : Check if the year is a leap year using below logic
 # Step 3: Check if year is divisible by 4. If divisible perform step 4.  If not divisible print "year is not leap year as it's not divisible by 4".
-# step 4 : Check if year is divisible by 100. If no, year is a leap year. If yes, perform Step 5.
-# step 5 : Check if year is divisible by 400. If yes, print year is a leap year. If no, print year is not a leap year
+
+if (year%4) == 0:
+    if (year%100) == 0:
+        if (year%400) == 0:
+            print(year, " is a leap year as it's divisible by 400")
+        else: print(year, "is not a leap year as it's divisible by 100 and not 400")
+    else: print(year, "is a leap year as it's divisible by 4 and not divisible by 100 or 400")
+else: print(year, "is not a leap year as it's not divisible by 4")
+
+
 
 
 
